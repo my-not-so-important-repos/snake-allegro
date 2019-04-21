df5265912afa8431d0ac132faeaef2bb304906c3
Heitor Adão Júnior <heitor.adao@gmail.com>
2019-04-20 21:55:42 -0300

    Version 6. Big change in the chipset.

--- s.c
+++ s.c
@@ -14,15 +14,17 @@
 
 #define DESENHAR_PEDACO_COBRA 1
 #define APAGAR_PEDACO_COBRA 2
-
+#define DESENHAR_CABECA 3
 
 int main(void);
 void mover_cobra(void);
 void desenhar_cobra(char);
 
 char tabuleiro[33][23], x, y, tamanho_cobra, direcao, game_over;
-BITMAP *chao, *parede, *maca, *spider,
-       *cabeca, *vertebrahorizontal, *vertebravertical, *cauda,
+BITMAP *chao, *parede, *maca,
+       *cabecacima, *cabecabaixo, *cabecaesquerda, *cabecadireita,
+       *caudacima, *caudabaixo, *caudaesquerda, *caudadireita,
+       *vertebrahorizontal, *vertebravertical,
        *vertebrasuperiordireita, *vertebrasuperioresquerda,
        *vertebrainferiordireita, *vertebrainferioresquerda;
 struct vertebra{
@@ -32,7 +34,7 @@ struct vertebra{
 
 
 int main(){
-  char temp[80];
+  char temp[40];
   char tecla;
   int i;
   BITMAP *chipset;
@@ -47,14 +49,42 @@ int main(){
   chipset = load_bitmap("chip1.bmp", paleta);
   set_palette(paleta);
   
-  chao   = create_bitmap(32, 32);
-  parede = create_bitmap(32, 32);
-  maca   = create_bitmap(32, 32);
-  spider = create_bitmap(32, 32);
+  chao										 = create_bitmap(32, 32);
+  parede									 = create_bitmap(32, 32);
+  maca										 = create_bitmap(32, 32);
 
-  blit(chipset, parede,   0, 0, 0, 0, 32, 32);
-  blit(chipset,   chao,  96, 0, 0, 0, 32, 32);
-  blit(chipset, spider, 128, 0, 0, 0, 32, 32);
+  cabecacima               = create_bitmap(32, 32);
+  cabecabaixo              = create_bitmap(32, 32);
+  cabecaesquerda           = create_bitmap(32, 32);
+  cabecadireita            = create_bitmap(32, 32);
+
+  caudacima                = create_bitmap(32, 32);
+  caudabaixo               = create_bitmap(32, 32);
+  caudaesquerda            = create_bitmap(32, 32);
+  caudadireita             = create_bitmap(32, 32);
+  
+  vertebrahorizontal			 = create_bitmap(32, 32);
+  vertebravertical				 = create_bitmap(32, 32);
+  vertebrainferiordireita	 = create_bitmap(32, 32);
+  vertebrainferioresquerda = create_bitmap(32, 32);
+  vertebrasuperiordireita  = create_bitmap(32, 32);
+  vertebrasuperioresquerda = create_bitmap(32, 32);
+
+
+  blit(chipset, parede,  0, 0, 0, 0, 32, 32);
+  blit(chipset,   chao, 96, 0, 0, 0, 32, 32);
+
+  blit(chipset, cabecacima,      0, 64, 0, 0, 32, 32);
+  blit(chipset, cabecadireita,  32, 64, 0, 0, 32, 32);
+  blit(chipset, cabecabaixo,    64, 64, 0, 0, 32, 32);
+  blit(chipset, cabecaesquerda, 96, 64, 0, 0, 32, 32);
+
+  
+  blit(chipset,  caudacima,      0, 96, 0, 0, 32, 32);
+  blit(chipset,  caudadireita,  32, 96, 0, 0, 32, 32);
+  blit(chipset,  caudabaixo,    64, 96, 0, 0, 32, 32);
+  blit(chipset,  caudaesquerda, 96, 96, 0, 0, 32, 32);
+
 
   tamanho_cobra = 3;
   cobra[0].x = 12;
@@ -68,6 +98,8 @@ int main(){
 
   game_over = 0;
   direcao = DIREITA;
+
+  destroy_bitmap(chipset); // para liberar mem¢ria
   
   /************************************************************************/
   /* fazer uma condi‡Æo que verifica se o usuario digitou um parƒmetro ao */
@@ -113,7 +145,7 @@ int main(){
           blit(parede, screen, 0, 0, (x - 1) * 32, (y - 1) * 32, parede->w, parede->h);
           break;
         case ' ':
-          blit(chao, screen, 0, 0, (x-1) * 32, (y - 1) * 32, parede->w, parede->h);
+          blit(chao, screen, 0, 0, (x - 1) * 32, (y - 1) * 32, parede->w, parede->h);
           break;
       }
     }
@@ -122,10 +154,12 @@ int main(){
   /****************************/
   /* In¡cio do loop principal */
   /****************************/
+  
   while(!game_over){
 
+    
     mover_cobra();
-    usleep(500000); //muito r pido
+    usleep(500000); //lento
     
     while(kbhit()){
       tecla = getch();
@@ -151,6 +185,13 @@ int main(){
 }
 END_OF_MAIN();
 
+
+
+
+
+
+
+
 void mover_cobra(){
   char i;
 
@@ -163,9 +204,9 @@ void mover_cobra(){
     cobra[i] = cobra[i - 1];
   }
 
-  /*************************************************/
-  /* Atualiza os bitmaps do rabo, pesco‡o e cabe‡a */
-  /*************************************************/
+  /*****************************************/
+  /* Atualiza os bitmaps do rabo e pesco‡o */
+  /*****************************************/
 	//cobra[tamanho_cobra-1].bitmap = ' ';
 	//cobra[1].bitmap = ' ';
 
@@ -174,17 +215,21 @@ void mover_cobra(){
   /********************************/
   switch(direcao){
     case CIMA:
-			cobra[0].y = cobra[0].y - 1;
+			cobra[0].y--;
+			cobra[0].bitmap = ''; //30
       break;
     case BAIXO:
-      cobra[0].y = cobra[0].y + 1;
+      cobra[0].y++;
+      cobra[0].bitmap = ''; //31
       break;
     case ESQUERDA:
-    	cobra[0].x = cobra[0].x - 1;
-      break;
+			cobra[0].x--;
+			cobra[0].bitmap = ''; //17
+			break;
     case DIREITA:
-    	cobra[0].x = cobra[0].x + 1;
-      break;
+			cobra[0].x++;
+			cobra[0].bitmap = ''; //16
+			break;
   }
 
   if(tabuleiro[cobra[0].x][cobra[0].y] == ' '){
@@ -195,28 +240,59 @@ void mover_cobra(){
 
 
 
+
+
+
+
+
 void desenhar_cobra(char operacao){
   switch(operacao){
     case DESENHAR_PEDACO_COBRA:
       //desenhar a cabe‡a
-      blit(spider, screen, 0, 0, (cobra[0].x - 1) * 32, (cobra[0].y - 1) * 32, parede->w, parede->h);
+      switch(cobra[0].bitmap){
+        case '': //30
+          blit(cabecacima, screen, 0, 0, (cobra[0].x - 1) * 32, (cobra[0].y - 1) * 32, parede->w, parede->h);
+          break;
+        case '': //31
+          blit(cabecabaixo, screen, 0, 0, (cobra[0].x - 1) * 32, (cobra[0].y - 1) * 32, parede->w, parede->h);
+          break;
+        case '': //17
+          blit(cabecaesquerda, screen, 0, 0, (cobra[0].x - 1) * 32, (cobra[0].y - 1) * 32, parede->w, parede->h);
+          break;
+        case '': //16
+          blit(cabecadireita, screen, 0, 0, (cobra[0].x - 1) * 32, (cobra[0].y - 1) * 32, parede->w, parede->h);
+          break;
+      }
 
       //desenhar o pesco‡o
-      blit(spider, screen, 0, 0, (cobra[1].x - 1) * 32, (cobra[1].y - 1) * 32, parede->w, parede->h);
+      //BUG: a cauda est  na mesma posicao do pesco‡o
+      blit(caudacima, screen, 0, 0, (cobra[1].x - 1) * 32, (cobra[1].y - 1) * 32, parede->w, parede->h);
 
-      //desenhar o rabo
-      blit(spider, screen, 0, 0, (cobra[tamanho_cobra - 1].x - 1) * 32, (cobra[tamanho_cobra - 1].y - 1) * 32, parede->w, parede->h);
+      //desenhar a cauda
+      switch(cobra[tamanho_cobra - 1].bitmap){
+	      case '': //24
+  	    	blit(caudacima, screen, 0, 0, (cobra[tamanho_cobra - 1].x - 1) * 32, (cobra[tamanho_cobra - 1].y - 1) * 32, parede->w, parede->h);
+    	    break;
+      	case '': //25
+      		blit(caudabaixo, screen, 0, 0, (cobra[tamanho_cobra - 1].x - 1) * 32, (cobra[tamanho_cobra - 1].y - 1) * 32, parede->w, parede->h);
+	        break;
+/*  	    case '': //26
+    	  	blit(caudadireita, screen, 0, 0, (cobra[tamanho_cobra - 1].x - 1) * 32, (cobra[tamanho_cobra - 1].y - 1) * 32, parede->w, parede->h);
+      	  break;
+        */
+	      case '': //27
+  	    	blit(caudaesquerda, screen, 0, 0, (cobra[tamanho_cobra - 1].x - 1) * 32, (cobra[tamanho_cobra - 1].y - 1) * 32, parede->w, parede->h);
+    	    break;
+      }
       break;
+      
     case APAGAR_PEDACO_COBRA:
-			//Apagar a cabe‡a
-			blit(chao, screen, 0, 0, (cobra[0].x - 1) * 32, (cobra[0].y - 1) * 32, parede->w, parede->h);
-
-			//Apagar o pesco‡o
-			blit(chao, screen, 0, 0, (cobra[1].x - 1) * 32, (cobra[1].y - 1) * 32, parede->w, parede->h);
-
-			//apagar o rabo
-      blit(chao, screen, 0, 0, (cobra[tamanho_cobra - 1].x -1) * 32, (cobra[tamanho_cobra - 1].y - 1) * 32, parede->w, parede->h);
-
+			blit(chao, screen, 0, 0, (cobra[0].x - 1) * 32, (cobra[0].y - 1) * 32, chao->w, chao->h); //Apagar a cabe‡a
+			blit(chao, screen, 0, 0, (cobra[1].x - 1) * 32, (cobra[1].y - 1) * 32, chao->w, chao->h); //Apagar o pesco‡o
+      blit(chao, screen, 0, 0, (cobra[tamanho_cobra - 1].x -1) * 32, (cobra[tamanho_cobra - 1].y - 1) * 32, chao->w, chao->h); //apagar o rabo
+      break;
+      
+    case DESENHAR_CABECA:
       break;
   }
 }
